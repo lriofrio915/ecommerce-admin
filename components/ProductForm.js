d1786dc30a5eedcbd8b1ff87a78824d8f5ae149b
luisRiofrio <lriofrio915@gmail.com>
2023-05-20 14:00:59 -0400

    upload fotos v2.0

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Spinner from "./Spinner";
 
 export default function ProductForm({
   _id, 
@@ -15,6 +16,7 @@ export default function ProductForm({
   const [price, setPrice] = useState(existingPrice || '');
   const [images,setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
+  const [isUpLoading, setIsUpLoading] = useState(false);
   const router = useRouter();
 
   async function saveProduct(ev){
@@ -37,15 +39,16 @@ export default function ProductForm({
   async function uploadImages(ev){
     const files = ev.target?.files;
     if (files?.length > 0) {
+      setIsUpLoading(true); 
       const data = new FormData();
       for(const file of files) {
         data.append('file', file);
       }
-      const res = await fetch('/api/upload', {
-        method: 'POST',
-        body: data,
+      const res = await axios.post('/api/upload',data)
+      setImages(oldImages => {
+        return [...oldImages, ...res.data.links];
       });
-      console.log(res);
+      setIsUpLoading(false);
     }
   }
 
@@ -62,7 +65,17 @@ export default function ProductForm({
         <label>
           Fotos
         </label>
-        <div className="mb-2">
+        <div className="mb-2 flex flex-wrap gap-1">
+          {!!images?.length && images.map(link =>(
+            <div key={link} className="h-24">
+              <img src={link} alt="image product" className="rounded-lg"/>
+            </div>
+          ))}
+          {isUpLoading && (
+            <diV className="h-24 p-1 flex items-center">
+              <Spinner/>
+            </diV>
+          )}
           <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 bg-gray-200 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" /></svg>
             <div>
@@ -70,11 +83,6 @@ export default function ProductForm({
             </div>
             <input type="file" onChange={uploadImages} className="hidden"/>
           </label>
-        </div>
-        <div className="">
-          {!images?.length && (
-            <div>No existen fotos en este producto</div>
-          )}
         </div>
         <label>Descripción</label>
         <textarea 
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
 import multiparty from 'multiparty';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
+
+const bucketName = 'vaper-next-ecommerce';
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -6,12 +11,36 @@ export default async function handle(req, res) {
     form.parse(req, (err, fields, files) => {
       if (err) reject(err);
       resolve({fields, files});
-      
     });
 
   });
   console.log('length:', files.file.length);
-  return res.json('ok');
+
+  const client = new S3Client({
+    region: 'us-east-2',
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const links = [];
+
+  for(const file of files.file){
+    const ext = file.originalFilename.split('.').pop();
+    const newFilename = Date.now() + '.' + ext;
+
+    await client.send(new PutObjectCommand({
+      Bucket: bucketName,
+      Key: newFilename,
+      Body:fs.readFileSync(file.path),
+      ACL: 'public-read',
+      ContentType: mime.lookup(file.path),
+    }));
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link);
+  }
+  return res.json({links});
 }
 
 export const config = {
