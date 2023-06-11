d134feebdea1e7ae181610ab18c0207d1d5e5021
luisRiofrio <lriofrio915@gmail.com>
2023-06-11 10:49:24 -0400

    active color nav component

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,7 +27,7 @@ export const authOptions = {
 
 export default NextAuth(authOptions)
 
-export async function isAdminRequest(req, res){
+export async function isAdminRequest(req,res){
   const session = await getServerSession(req, res, authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
