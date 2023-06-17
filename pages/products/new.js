026c6a6935f45606a195dc478afe9ccc4a7863f1
luisRiofrio <lriofrio915@gmail.com>
2023-06-17 12:05:26 -0400

    order page date column

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,5 +1,5 @@
-import Layout from "@/components/Layout";
-import ProductForm from "@/components/ProductForm";
+import Layout from "../../components/Layout";
+import ProductForm from "../../components/ProductForm";
 
 export default function NewProduct() {
   return (
